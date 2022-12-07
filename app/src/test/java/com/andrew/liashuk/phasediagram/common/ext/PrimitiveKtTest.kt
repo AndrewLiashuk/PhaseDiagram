f0012e1f62e479f5ac58cc96c3f0cf4d4cf38022
Andrew <andriili@backbase.com>
2022-12-07 17:43:02 +0200

    Create unit tests for `toPrettyString` ext

--- app/src/test/java/com/andrew/liashuk/phasediagram/common/ext/PrimitiveKtTest.kt
+++ app/src/test/java/com/andrew/liashuk/phasediagram/common/ext/PrimitiveKtTest.kt
@@ -1,4 +1,37 @@
 package com.andrew.liashuk.phasediagram.common.ext
 
-//TODO create tests
-class PrimitiveTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class PrimitiveTest {
+
+    @Test
+    fun `should return empty string on null`() {
+        val value: Double? = null
+        assertEquals("", value.toPrettyString())
+    }
+
+    @Test
+    fun `should return number without fractional part`() {
+        val value = 20.00
+        assertEquals("20", value.toPrettyString())
+    }
+
+    @Test
+    fun `should return number without last 0 in fractional part`() {
+        val value = 20.10
+        assertEquals("20.1", value.toPrettyString())
+    }
+
+    @Test
+    fun `should return number same as value`() {
+        val value = 20.11
+        assertEquals("20.11", value.toPrettyString())
+    }
+
+    @Test
+    fun `should return number even with 0 but only in the middle of fractional part`() {
+        val value = 20.101
+        assertEquals("20.101", value.toPrettyString())
+    }
+}
